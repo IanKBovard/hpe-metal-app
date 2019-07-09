9fb7d0e449837e44bfa83cd912c35792d9f6bc20
iankbovard <you@example.com>
2019-07-09 15:21:26 -0500

    packages added

---
+++ src/lambda/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+router.get('/ping', (req, res) => {
+  res.send('pong!');
+});
