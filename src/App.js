499f35350c5d8c84957a31e021c0f08fab4aa7f5
iankbovard <you@example.com>
2019-07-09 15:51:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        </object>
-          Learn React
+        TEST!!
         </a>
       </header>
     </div>
