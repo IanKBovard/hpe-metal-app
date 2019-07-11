9192ed27a89214c93d4329ba783a8da92be2e1b3
iankbovard <you@example.com>
2019-07-11 15:40:18 -0500

    text added

--- src/containers/loading/index.js
+++ src/containers/loading/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Box, Text } from 'grommet';
-import { HandDiv, AccentDiv, ImageDiv } from './style';
+import { Box, Anchor } from 'grommet';
+import { HandDiv, AccentDiv, ImageDiv, AccentDivPink, TitleText, TextDiv, InfoText } from './style';
 
 const Loading = () => {
   return (
-    <Box fill direction="row-responsive">
-      <Box basis="1/3">TEXT GOES HERE!</Box>
-      <ImageDiv basis="2/3">
+    <Box fill direction="row-responsive" background="#1F2532" pad={{ top: 'xlarge', left: 'xlarge' }}>
+      <TextDiv basis="1/4" direction="column" pad={{ top: 'xlarge'}}>
+        <Box>
+          <TitleText>Get ready for</TitleText>
+          <TitleText>something</TitleText>
+          <TitleText>wicked</TitleText>
+        </Box>
+        <Box margin={{ top: 'large' }}>
+          <InfoText>Taking you to your server! Can't wait? No</InfoText>
+          <InfoText>prob, take me to <Anchor label="my portal"/>.</InfoText>
+        </Box>
+      </TextDiv>
+      <ImageDiv basis="3/4">
         <AccentDiv src="./img/loading/metal-red.svg"/>
-        <AccentDiv src="./img/loading/metal-pink.svg"/>
+        <AccentDivPink src="./img/loading/metal-pink.svg"/>
         <HandDiv src="./img/loading/metal-hand.svg"/>
         <AccentDiv src="./img/loading/metal-yellow.svg"/>
       </ImageDiv>
--- src/containers/loading/style.js
+++ src/containers/loading/style.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { Box, Image } from 'grommet';
+import { Box, Image, Text, Heading } from 'grommet';
 
 export const HandDiv = styled(Image)`
   position:absolute;
-  width: 25%;
+  width: 30%;
   height: auto;
-  right: 40%;  
+  right: 37%;  
   bottom: 0;
 `;
 
@@ -17,7 +17,33 @@ export const AccentDiv = styled(Image)`
   bottom: 0;
 `;
 
+export const AccentDivPink = styled(Image)`
+  position:absolute;
+  width: 100%;
+  height: auto;
+  right: 0;
+  bottom: 0;
+  mix-blend-mode: overlay;
+`;
+
 export const ImageDiv = styled(Box)`
   position: relative;
   padding-top: 56.25%;
+`;
+
+export const TitleText = styled(Heading)`
+  font-size: 7vw;
+  font-weight: 900;
+  line-height: .8em;
+  margin: 0px;
+`;
+
+export const InfoText = styled(Text)`
+  font-size: 2.5vw;
+  font-weight: 200;
+  line-height: .8em;
+`;
+
+export const TextDiv = styled(Box)`
+  white-space: nowrap;
 `;
