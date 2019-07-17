2f0268fc7aebff452f75d09fdc4404e27a859625
iankbovard <you@example.com>
2019-07-17 18:14:34 -0500

    test

--- src/components/svg/Hand.js
+++ src/components/svg/Hand.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Hand = () => {
   return (
     <svg
+      height="100%"
       viewBox="0 0 342 943"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
