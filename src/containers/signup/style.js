7dfbaeec7fb980cb5e723f8949f06aca44b1a15c
iankbovard <you@example.com>
2019-07-26 09:59:09 -0500

    ie11 overflow test

--- src/containers/signup/style.js
+++ src/containers/signup/style.js
@@ -4,6 +4,9 @@ import { Box, Text, Heading, Anchor, TextInput, Button } from 'grommet';
 export const Layout = styled(Box)`
   position: relative;
   padding-top: 56.25%;
+  @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+    overflow-x: hidden;
+  }
 `;
 
 export const TitleText = styled(Heading)`
